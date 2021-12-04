0da97667a7cc2e99b62fbee45538f9cc3f6bf3fa
mnito <mpnitowski@gmail.com>
2021-12-04 15:06:42 -0500

    Use word-safe base32 alphabet for url generation

--- src/urlgen.cc
+++ src/urlgen.cc
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2021 Michael P. Nitowski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <algorithm>
 #include <chrono>
-#include <iostream>
 #include <sstream>
 #include <tuple>
 
@@ -12,8 +27,8 @@ uint64_t generate_url_id() {
     return url_id;
 }
 
-std::string a = "0123456789befhjmnrstvwyz";
-size_t a_size = 24;
+std::string a = "23456789CFGHJMPQRVWXcfghjmpqrvwx";
+size_t a_size = 32;
 
 std::string encode_url_id(uint64_t url_id) {
     std::stringstream s;
@@ -26,26 +41,25 @@ std::string encode_url_id(uint64_t url_id) {
     return str;
 }
 
-static uint64_t pow24_cache[14] = {
+static uint64_t pow32_cache[14] = {
     1,
-    24,
-    576,
-    13824,
-    331776,
-    7962624,
-    191102976,
-    4586471424,
-    110075314176,
-    2641807540224,
-    63403380965376,
-    1521681143169024,
-    36520347436056576,
-    876488338465357824,
+    32,
+    1024,
+    32768,
+    1048576,
+    33554432,
+    1073741824,
+    34359738368,
+    1099511627776,
+    35184372088832,
+    1125899906842624,
+    36028797018963968,
+    1152921504606846976
 };
 
-uint64_t pow24(int n) {
+uint64_t pow32(int n) {
     if (n > 13) return -1;
-    return pow24_cache[n];
+    return pow32_cache[n];
 }
 
 uint64_t decode_url_id(std::string url_id_str) {
@@ -53,7 +67,7 @@ uint64_t decode_url_id(std::string url_id_str) {
 
     size_t i = 0;
     for (auto &l : url_id_str) {
-        id += a.find(l) * pow24(url_id_str.size() - (i + 1));
+        id += a.find(l) * pow32(url_id_str.size() - (i + 1));
         i++;
     }
     return id;
